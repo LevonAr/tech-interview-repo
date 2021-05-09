f7bcb0ac324af3dcb4eb567b9d195e2dda21a81b
LevonAr <levonrm10@gmail.com>
2021-05-09 14:48:34 -0700

    added Binary Search

---
+++ src/algorithms/search/binary-search/binarySearch.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function searchInsert(nums, target) {
+  let startIndex = 0;
+  let endIndex = nums.length - 1;
+
+  // split array
+  while (startIndex < endIndex) {
+    // calculate middle index
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+    // check if the middle index value is target
+    if (target == nums[middleIndex]) {
+      return middleIndex;
+    }
+
+    // pick which side to look in
+    if (target > nums[middleIndex]) {
+      startIndex = middleIndex + 1;
+    } else {
+      endIndex = middleIndex - 1;
+    }
+  }
+
+  return -1;
+}
